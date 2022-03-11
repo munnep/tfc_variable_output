a74b297e3accaefd3e6ccd0846dcc0c296da2a4f
munnep <patrick.munne@hashicorp.com>
2022-03-11 10:36:24 +0100

    example code

---
+++ main.tf
@@ -0,0 +1,11 @@
+output "default_variable" {
+    value = var.default_variable
+}
+
+output "workspace_variable" {
+    value = var.workspace_variable
+}
+
+output "global_variable" {
+    value = var.global_variable
+}
---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "default_variable" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "this is the default variable output"
+}
+
+variable "workspace_variable" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "not set at the moment"
+}
+
+variable "global_variable" {
+    type = string
+    description = "(optional) describe your variable"
+    default = "not set at the moment"
+}
